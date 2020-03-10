1c7114e5da34b1f52b93b49c8c07981d52226083
rau <rauschie.j@gmail.com>
2020-03-10 10:24:02 +0100

    add AuthenticationService and HttpInterceptor

--- moovsmart/src/app/app.module.ts
+++ moovsmart/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PropertyFormComponent } from './components/property-form/property-form.component';
 import { PropertyDetailsComponent } from './components/property-details/property-details.component';
 import { PropertyListComponent } from './components/property-list/property-list.component';
 import { UserregisterFormComponent } from './components/userregister-form/userregister-form.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
+import { HttpInterceptorService } from './services/http-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,13 @@ import { UserLoginComponent } from './components/user-login/user-login.component
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
