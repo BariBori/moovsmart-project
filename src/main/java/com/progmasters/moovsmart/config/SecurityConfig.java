9ee2bb5477790b9fb6a4af2e2cb7d492ec7e4bc8
rau <rauschie.j@gmail.com>
2020-03-10 11:59:37 +0100

    edit SecurityConfig

--- src/main/java/com/progmasters/moovsmart/config/SecurityConfig.java
+++ src/main/java/com/progmasters/moovsmart/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.progmasters.moovsmart.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,6 +30,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(detailsService)
                 .passwordEncoder(passwordEncoder());
     }
+
+    //TODO constrain further
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.cors().and()
+                .csrf().disable();
+    }
+
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
--- src/main/java/com/progmasters/moovsmart/controller/UserController.java
+++ src/main/java/com/progmasters/moovsmart/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,5 +32,11 @@ public class UserController {
     public ResponseEntity<Void> registerUser(@Valid @RequestBody UserForm userFormData) {
         service.registerUser(userFormData);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/authenticate")
+    @Secured("USER")
+    public ResponseEntity<Void> authenticateUser() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
