c21cba45c80cb9e0b0895513101a88a342647a7a
Bozsó-Fort Zsuzsanna <fortzsu@gmail.com>
2020-03-10 09:49:53 +0100

    Add UserForm FE

--- src/main/java/com/progmasters/moovsmart/repository/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.progmasters.moovsmart.repository;
-
-import com.progmasters.moovsmart.domain.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
