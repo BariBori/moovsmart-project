831ab5d765263ce7264e21cc2a9b84f13cfa1723
rau <rauschie.j@gmail.com>
2020-03-11 17:44:01 +0100

    add user-home component

--- moovsmart/src/app/app.module.ts
+++ moovsmart/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+<<<<<<< Updated upstream
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
@@ -16,6 +17,24 @@ import {UserLoginComponent} from './components/user-login/user-login.component';
 import {HttpInterceptorService} from './services/http-interceptor.service';
 import {SearchComponent} from './components/search/search.component';
 import {UserHomeComponent} from './components/user-home/user-home.component';
+=======
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PropertyFormComponent } from './components/property-form/property-form.component';
+import { PropertyDetailsComponent } from './components/property-details/property-details.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
+import { UserregisterFormComponent } from './components/userregister-form/userregister-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { HttpInterceptorService } from './services/http-interceptor.service';
+import { SearchComponent } from './components/search/search.component';
+import { UserHomeComponent } from './components/user-home/user-home.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
