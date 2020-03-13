a2a4bea93b8a4702364ff14d8b1b293576378b1c
pal.orsolya@gmail.com <pal.orsolya@gmail.com>
2020-03-13 18:07:26 +0100

    modify property-form.ts add cloudinary module

--- moovsmart/src/app/components/property-form/property-form.component.ts
+++ moovsmart/src/app/components/property-form/property-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, NgZone, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 import { PropertyService } from '../../services/property.service';
 import { Router } from '@angular/router';
@@ -9,6 +9,9 @@ import {ParkingTypeOptionItemModel} from "../../models/parkingTypeOptionItem.mod
 import {FormInitDataModel} from "../../models/formInitDataModel";
 import {MapsAPILoader} from "@agm/core";
 import {HttpClient} from "@angular/common/http";
+import {FileUploader, FileUploaderOptions, ParsedResponseHeaders} from "ng2-file-upload";
+import {Cloudinary} from "@cloudinary/angular-5.x";
+
 
 @Component({
   selector: 'app-property-form',
@@ -16,11 +19,18 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./property-form.component.css']
 })
 export class PropertyFormComponent implements OnInit {
-  propertyType: Array<PropertyTypeOptionItemModel>;
-  propertyConditionType: Array<PropertyConditionTypeOptionItemModel>;
-  parkingType: Array<ParkingTypeOptionItemModel>;
+  //------Cloudinary file upload------------
+  @Input()
+  responses: Array<any>;
+  listOfImages: Array<any> = [];
+  private hasBaseDropZoneOver: boolean = false;
+  uploader: FileUploader;
+  private imgTitle: string;
+  //---------------------------------------
 
-    private geoCoder;
+
+  //------Google Maps------------
+  private geoCoder;
     zoom: number;
     address: string;
     city: string;
@@ -33,33 +43,37 @@ export class PropertyFormComponent implements OnInit {
     @ViewChild('search')
     public searchElementRef: ElementRef;
 
+    //---------------------------------
+
+  propertyType: Array<PropertyTypeOptionItemModel>;
+  propertyConditionType: Array<PropertyConditionTypeOptionItemModel>;
+  parkingType: Array<ParkingTypeOptionItemModel>;
+
     propertyForm = this.formBuilder.group({
-    price: [0 ,Validators.required],
-    listOfImages: [null],
-
-    propertyType: ['',Validators.required],
-    propertyConditionType: ['',Validators.required],
-    parkingType: ['',Validators.required],
-    title: ['',Validators.required],
-
-
-    area: [0,Validators.required],
-    numberOfRooms: [0,Validators.required],
-
-    elevator: [false],
-    balcony: [false],
-
-    description: ['',Validators.required],
-
-    advertStatus: ['FORAPPROVAL'],
+      advertStatus: ['FORAPPROVAL'],
 
       address: [''],
       city: [''],
       street: [''],
       district: [''],
-      postalCode: ['']
+      postalCode: [''],
 
+      title: ['',Validators.required],
+      price: [0 ,Validators.required],
 
+      area: [0,Validators.required],
+      numberOfRooms: [0,Validators.required],
+
+      propertyType: ['',Validators.required],
+      propertyConditionType: ['',Validators.required],
+      parkingType: ['',Validators.required],
+
+      elevator: [false],
+      balcony: [false],
+
+      description: ['',Validators.required],
+
+      listOfImages: [null],
   });
 
 
@@ -67,10 +81,13 @@ export class PropertyFormComponent implements OnInit {
     private formBuilder: FormBuilder,
     private propertyService: PropertyService,
     private router: Router,
-
     private httpClient: HttpClient,
+    //-----Google Maps----
     private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone
+    private ngZone: NgZone,
+    //----Cloudinary-----
+    //private zone: NgZone,
+    private cloudinary: Cloudinary,
   ) { }
 
   ngOnInit() {
@@ -81,9 +98,118 @@ export class PropertyFormComponent implements OnInit {
         this.parkingType = initData.parkingType;
       },
       error => console.warn(error)
-    )
+    );
 
 
+
+     //----------CLOUDINARY----------------------
+    // Create the file uploader, wire it to upload to your account
+    const config = this.cloudinary.config();
+    const cloud_name = "dqmt1lieq";
+    const uploaderOptions: FileUploaderOptions = {
+      url: "https://api.cloudinary.com/v1_1/"+  this.cloudinary.config().cloud_name+"/image/upload",
+
+      // Upload files automatically upon addition to upload queue
+      autoUpload: true,
+      // Use xhrTransport in favor of iframeTransport
+      isHTML5: true,
+      // Calculate progress independently for each uploaded file
+      removeAfterUpload: true,
+      // XHR request headers
+      headers: [
+        {
+          name: 'X-Requested-With',
+          value: 'XMLHttpRequest'
+        }
+      ]
+    };
+
+    this.uploader = new FileUploader(uploaderOptions);
+
+    this.uploader.onBuildItemForm = (fileItem: any, form: FormData): any => {
+      // Add Cloudinary unsigned upload preset to the upload form
+      const upload_preset = "s1jujbuu";
+      form.append('upload_preset', this.cloudinary.config().upload_preset);
+
+      let tags = 'mypropertyalbum';
+      if(this.imgTitle){
+        form.append('context', `photo=${this.imgTitle}`);
+        tags = `myphotoalbum,${this.imgTitle}`;
+      }
+      // Add  folder name
+      form.append('folder', 'property_album');
+
+      // Add custom tags
+      form.append('tags', tags);
+
+      // Add file to upload
+      form.append('file', fileItem);
+
+      // Use default "withCredentials" value for CORS requests
+      fileItem.withCredentials = false;
+      return { fileItem, form };
+    };
+
+    // Insert or update an entry in the responses array
+    const upsertResponse = fileItem => {
+
+      // Run the update in a custom zone since for some reason change detection isn't performed
+      // as part of the XHR request to upload the files.
+      // Running in a custom zone forces change detection
+      this.ngZone.run(() => {
+        // Update an existing entry if it's upload hasn't completed yet
+
+        // Find the id of an existing item
+        const existingId = this.responses.reduce((prev, current, index) => {
+          if (current.file.name === fileItem.file.name && !current.status) {
+            return index;
+          }
+          return prev;
+        }, -1);
+        if (existingId > -1) {
+          // Update existing item with new data
+          this.responses[existingId] = Object.assign(this.responses[existingId], fileItem);
+        } else {
+          // Create new response
+
+          this.responses.push(fileItem);
+        }
+        console.log(this.responses);
+
+        //fill listOfImages array
+        this.listOfImages.push(fileItem.data.url);
+        this.listOfImages= this.listOfImages.filter(function (el) {
+          return el != null;
+        });
+        console.log(this.listOfImages);
+      });
+
+    };
+
+    // Update model on completion of uploading a file
+    this.uploader.onCompleteItem = (item: any, response: string, status: number, headers: ParsedResponseHeaders) =>
+      upsertResponse(
+        {
+          file: item.file,
+          status,
+          data: JSON.parse(response)
+        }
+      );
+
+    // Update model on upload progress event
+    this.uploader.onProgressItem = (fileItem: any, progress: any) =>
+      upsertResponse(
+        {
+          file: fileItem.file,
+          progress,
+          data: {}
+        }
+      );
+
+    //---------CLOUDINARY END--------------------
+
+
+    //---------GOOGLE MAPS----------------------
     //load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
 
@@ -102,9 +228,6 @@ export class PropertyFormComponent implements OnInit {
             return;
           }
 
-          //set latitude, longitude and zoom
-          //this.latitude = place.geometry.location.lat();
-          //this.longitude = place.geometry.location.lng();
           this.address = place.formatted_address;
           this.placeId = place.place_id;
           this.addressComponent = place.address_components;
@@ -138,11 +261,66 @@ export class PropertyFormComponent implements OnInit {
         });
       });
     });
+  } //NGONINIT END
 
+  //-----------GOOGLE MAPS------------
+  clearAddressDetails() {
+    this.street='';
+    this.postalCode='';
+    this.address='';
+    this.city='';
+    this.district='';
+  }
 
+  //---------GOOGLE MAPS END-----------
+
+  //----------CLOUDINARY----------------
+
+  updateTitle(value: string) {
+    this.imgTitle = value;
+  }
+
+  // Delete an uploaded image
+  // Requires setting "Return delete token" to "Yes" in your upload preset configuration
+  // See also https://support.cloudinary.com/hc/en-us/articles/202521132-How-to-delete-an-image-from-the-client-side-
+  deleteImage = function (data: any, index: number) {
+    const url = `https://api.cloudinary.com/v1_1/${this.cloudinary.config().cloud_name}/delete_by_token`;
+    const headers = new Headers({ 'Content-Type': 'application/json', 'X-Requested-With': 'XMLHttpRequest' });
+    const options = { headers: headers };
+    const body = {
+      token: data.delete_token
+    };
+    console.log(data);
+    console.log(data.delete_token);
+    this.http.post(url, body, options).subscribe(response => {
+      console.log(`Deleted image - ${data.public_id} ${response.result}`);
+      // Remove deleted item for responses
+      this.responses.splice(index, 1);
+    });
+  };
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  getFileProperties(fileProperties: any) {
+    // Transforms Javascript Object to an iterable to be used by *ngFor
+    if (!fileProperties) {
+      return null;
+    }
+    let fileObject = Object.keys(fileProperties)
+      .map((key) => ({ 'key': key, 'value': fileProperties[key] }));
+    return fileObject;
 
   }
 
+
+  deleteImgFromArray(array: any, i: number) {
+    delete array[i];
+  }
+
+
+  //--------CLOUDINARY END-------------
 
 
   submit = () => {
@@ -159,11 +337,5 @@ export class PropertyFormComponent implements OnInit {
     )
   };
 
-  clearAddressDetails() {
-    this.street='';
-    this.postalCode='';
-    this.address='';
-    this.city='';
-    this.district='';
-  }
+
 }
