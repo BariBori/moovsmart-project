7041a793e65fa93e2c0957ca7dcbe90edd7c84c5
rau <rauschie.j@gmail.com>
2020-03-16 15:31:44 +0100

    add UserFormValidator logic and User entity email regexp

--- src/main/java/com/progmasters/moovsmart/domain/User.java
+++ src/main/java/com/progmasters/moovsmart/domain/User.java
@@ -1,7 +1,6 @@
 package com.progmasters.moovsmart.domain;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,19 +9,17 @@ import java.util.List;
 @Entity
 public class User {
 
+    private static final String EMAIL_REGEXP =
+            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-
     @Column(unique = true)
-    @Pattern(regexp = EMAIL_REGEX)
+    @Pattern(regexp = EMAIL_REGEXP)
     private String email;
     private String passwordHash;
     @OneToOne(mappedBy = "user")
     private PersonalDetails personalDetails;
-    @NotEmpty
     @ElementCollection(targetClass = UserRole.class)
     @Enumerated(EnumType.STRING)
     private List<UserRole> userRoles;
--- src/main/java/com/progmasters/moovsmart/validation/UserFormValidator.java
+++ src/main/java/com/progmasters/moovsmart/validation/UserFormValidator.java
@@ -1,10 +1,23 @@
 package com.progmasters.moovsmart.validation;
 
 import com.progmasters.moovsmart.dto.UserForm;
+import com.progmasters.moovsmart.service.UserService;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.util.function.Predicate;
+
+@Component
 public class UserFormValidator implements Validator {
+    private UserService service;
+
+    public UserFormValidator(UserService service) {
+        this.service = service;
+    }
+
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.equals(UserForm.class);
@@ -12,6 +25,26 @@ public class UserFormValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+        Predicate<String> validEmail = email -> {
+            boolean valid;
+            try {
+                new InternetAddress(email).validate();
+                valid = true;
+            } catch (AddressException ex) {
+                valid = false;
+            }
+            return valid;
+        };
+        var userForm = (UserForm) target;
+        String email = userForm.getEmail();
+        if (Boolean.TRUE.equals(service.isEmailTaken(email))) {
+            errors.rejectValue("email", "moovsmart.user.email.alreadyTaken");
+        } else if (!validEmail.test(email)) {
+            errors.rejectValue("email", "moovsmart.user.email.invalid");
+        }
+        if (userForm.getPassword().length() < 4) {
+            errors.rejectValue("password", "moovsmart.user.password.invalid");
+        }
 
     }
 }
