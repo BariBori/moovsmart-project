b407498c14c53a64611787049a39cbeb2a02e47f
pal.orsolya@gmail.com <pal.orsolya@gmail.com>
2020-03-19 14:41:27 +0100

    modify navbar property-advert button with userService - authentication

--- moovsmart/src/app/components/navbar/navbar.component.ts
+++ moovsmart/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +10,28 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() {
+  userName: string;
+
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
+    this.userService.getCurrentUser.subscribe(
+      data => this.userName = data.userName
+    );
+  }
+
+  onSubmit(){
+    if(this.userName == null) {
+      this.router.navigate(['user-login']);
+    } else {
+      this.router.navigate(['property-form']);
+    }
   }
 
 }
