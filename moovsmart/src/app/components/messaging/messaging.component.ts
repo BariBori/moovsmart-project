6d16e2e992e3a274ac629eb0f5ec5238557481a1
rau <rauschie.j@gmail.com>
2020-03-25 13:50:44 +0100

    edit Topic Dto

--- moovsmart/src/app/components/messaging/messaging.component.ts
+++ moovsmart/src/app/components/messaging/messaging.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MessagingService } from 'src/app/services/messaging.service';
-import { UserService } from 'src/app/services/user.service';
+import { TopicMap } from 'src/app/models/messaging/TopicMap';
+import { MessageModel } from 'src/app/models/messaging/MessageModel';
 
 @Component({
   selector: 'app-messaging',
@@ -11,18 +12,30 @@ import { UserService } from 'src/app/services/user.service';
 export class MessagingComponent implements OnInit {
 
   message: FormControl;
+  topics: TopicMap;
+  messages: MessageModel[];
+  activeTopicId: number;
 
   constructor(
     private msgservice: MessagingService,
-    usrService: UserService
   ) {
     this.message = new FormControl('', Validators.required);
   }
   send() {
+    this.msgservice.sendMessage(this.message.value, this.activeTopicId)
+      .subscribe(success => this.message.setValue(''),
+        err => console.error(err));
+
   }
 
   ngOnInit(): void {
-    this.msgservice.fetcchAllTopics.subscribe(console.log, console.error)
+    this.msgservice.fetchAllTopics.subscribe(
+      response => this.topics = response,
+      err => console.error(err));
+  }
+  setActiveTopic(topicId: number): void {
+    this.activeTopicId = topicId;
+    console.log(this.activeTopicId);
   }
 
 }
