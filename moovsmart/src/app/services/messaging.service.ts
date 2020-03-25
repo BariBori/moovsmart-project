f59afd87194bf07d14c9ced8205a62e05976485a
rau <rauschie.j@gmail.com>
2020-03-25 17:48:39 +0100

    make msg list refresh upon send

--- moovsmart/src/app/services/messaging.service.ts
+++ moovsmart/src/app/services/messaging.service.ts
@@ -5,15 +5,16 @@ import { environment } from 'src/environments/environment';
 import { tap } from 'rxjs/operators';
 import { TopicModel } from '../models/messaging/TopicModel';
 import { TopicMap } from '../models/messaging/TopicMap';
+import { MessageModel } from '../models/messaging/MessageModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagingService {
 
-  private BASE_URL: string = environment.BASE_URL + '/api/topics'
+  private BASE_URL: string = environment.BASE_URL + '/api/topics';
   public subscribeToTopic: (advertId: number) => Observable<number>;
-  public sendMessage: (message: string, topicId: number) => Observable<void>;
+  public sendMessage: (message: string, topicId: number) => Observable<MessageModel>;
   public fetchTopic: (topicId: number) => Observable<TopicModel>;
   public fetchAllTopics: Observable<TopicMap>;
   constructor(
@@ -24,7 +25,7 @@ export class MessagingService {
         .pipe(tap(console.log, console.error));
 
     this.sendMessage = (message: string, topicId: number) =>
-      this.http.put<void>(this.BASE_URL + `/${topicId}`, message)
+      this.http.put<MessageModel>(this.BASE_URL + `/${topicId}`, message)
         .pipe(tap(console.log, console.error));
 
     this.fetchTopic = (topicId: number): Observable<TopicModel> =>
