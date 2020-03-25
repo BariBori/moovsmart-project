08a4440603153780a7d772cd5f90745a82941998
rau <rauschie.j@gmail.com>
2020-03-25 14:04:27 +0100

    enrich topicDto

--- moovsmart/src/app/models/messaging/TopicModel.ts
+++ moovsmart/src/app/models/messaging/TopicModel.ts
@@ -2,5 +2,8 @@ import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 export interface TopicModel {
     advertId: number;
+    advertTitle: string;
+    advertiser: string;
+    enquirer: string;
     messages: Message[];
 }
--- src/main/java/com/progmasters/moovsmart/dto/messaging/TopicDto.java
+++ src/main/java/com/progmasters/moovsmart/dto/messaging/TopicDto.java
@@ -8,11 +8,17 @@ import java.util.stream.Collectors;
 
 public class TopicDto {
     private Long advertId;
-    private List<MessageDto> messages;
 
+    private String advertTitle;
+    private String advertiser;
+    private String enquirer;
+    private List<MessageDto> messages;
     public static TopicDto fromTopic(Topic topic) {
         TopicDto result = new TopicDto();
-        result.advertId = topic.getAdvert().getId();
+        result.advertId=topic.getAdvert().getId();
+        result.advertTitle = topic.getAdvert().getTitle();
+        result.advertiser = topic.getAdvertiser().getUserName();
+        result.enquirer = topic.getEnquirer().getUserName();
         result.messages = topic.getMessages().stream()
                 .map(MessageDto::fromMessage)
                 .collect(Collectors.toList());
@@ -21,6 +27,18 @@ public class TopicDto {
 
     public Long getAdvertId() {
         return advertId;
+    }
+
+    public String getAdvertTitle() {
+        return advertTitle;
+    }
+
+    public String getAdvertiser() {
+        return advertiser;
+    }
+
+    public String getEnquirer() {
+        return enquirer;
     }
 
     public List<MessageDto> getMessages() {
