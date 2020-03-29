4de39e56e1b58641a807c0f0063757aa25397919
rau <rauschie.j@gmail.com>
2020-03-29 17:25:37 +0200

    init repo

--- src/main/java/com/progmasters/moovsmart/service/PropertyAdvertService.java
+++ src/main/java/com/progmasters/moovsmart/service/PropertyAdvertService.java
@@ -2,7 +2,7 @@ package com.progmasters.moovsmart.service;
 
 import com.progmasters.moovsmart.domain.*;
 import com.progmasters.moovsmart.domain.user.User;
-import com.progmasters.moovsmart.domain.user.UserDetailsImpl;
+import com.progmasters.moovsmart.domain.user.UserIdentifier;
 import com.progmasters.moovsmart.dto.*;
 import com.progmasters.moovsmart.repository.AdvertRepository;
 import com.progmasters.moovsmart.repository.UserRepository;
@@ -14,7 +14,6 @@ import javax.persistence.EntityNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,7 +29,7 @@ public class PropertyAdvertService {
         this.userRepository = userRepository;
     }
 
-    public void saveAdvert(PropertyAdvertFormData propertyAdvertFormData, UserDetailsImpl userDetails) {
+    public void saveAdvert(PropertyAdvertFormData propertyAdvertFormData, UserIdentifier userDetails) {
         Optional<User> user = userRepository.findByUserName(userDetails.getUsername());
         if(user.isPresent()) {
             User chosenUser = user.get();
