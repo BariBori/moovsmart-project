337f9c8395a9c11d1f9cf737dc00d8bce0d8bf7c
rau <rauschie.j@gmail.com>
2020-03-29 17:24:46 +0200

    rename

---
+++ src/main/java/com/progmasters/moovsmart/domain/messaging/Chat.java
@@ -0,0 +1,67 @@
+package com.progmasters.moovsmart.domain.messaging;
+
+
+import com.progmasters.moovsmart.domain.PropertyAdvert;
+import com.progmasters.moovsmart.domain.user.User;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Table(uniqueConstraints =
+        {@UniqueConstraint(columnNames = {"advert", "enquirer"})})
+@Entity
+public class Chat extends Conversation {
+    @OneToOne
+    private PropertyAdvert advert;
+    @OneToOne
+    private User enquirer;
+
+    @Entity
+    public static class View extends com.progmasters.moovsmart.domain.messaging.View<Chat> {
+        @OneToOne
+        private User partner;
+
+        public View() {
+        }
+
+        public View(User user, Chat chat) {
+            super(user, chat);
+            this.partner = user.getId().equals(chat.enquirer.getId())
+                    ? chat.advert.getUser()
+                    : chat.enquirer;
+        }
+
+        @Override
+        public Chat getConversation() {
+            return this.conversation;
+        }
+
+        public User getPartner() {
+            return partner;
+        }
+
+        public String getTitle() {
+            return this.conversation
+                    .getAdvert()
+                    .getTitle();
+        }
+    }
+
+    public Chat() {
+    }
+
+    public Chat(User enquirer, PropertyAdvert advert) {
+        this.enquirer = enquirer;
+        this.advert = advert;
+    }
+
+    public PropertyAdvert getAdvert() {
+        return advert;
+    }
+
+    public User getEnquirer() {
+        return enquirer;
+    }
+}
