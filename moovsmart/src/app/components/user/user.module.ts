5ebbdec2721f6b0ee27a88b4f0ee745f6bc24fd8
rau <rauschie.j@gmail.com>
2020-03-31 14:53:07 +0200

    add matBadge

--- moovsmart/src/app/components/user/user.module.ts
+++ moovsmart/src/app/components/user/user.module.ts
@@ -1,17 +1,18 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import {UserHomeComponent} from "./user-home/user-home.component";
+import { UserHomeComponent } from "./user-home/user-home.component";
 
 import { UserPropertyComponent } from './user-property/user-property.component';
-import {UserDetailsComponent} from "./user-details/user-details.component";
+import { UserDetailsComponent } from "./user-details/user-details.component";
 
-import {UserRoutingModule} from "./user-routing.module";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {faStar, faUser} from "@fortawesome/free-regular-svg-icons";
-import {faCity, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import { UserRoutingModule } from "./user-routing.module";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faCity, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   imports: [
@@ -19,16 +20,17 @@ import {faCity, faEnvelope} from "@fortawesome/free-solid-svg-icons";
     UserRoutingModule,
     MatSidenavModule,
     FontAwesomeModule,
+    MatBadgeModule
   ],
   declarations: [
     UserHomeComponent,
     UserDetailsComponent,
     UserPropertyComponent,
   ]
-  }
+}
 
 )
-export class UserModule{
+export class UserModule {
 
   constructor() {
     library.add(faStar, faUser, faCity, faEnvelope);
