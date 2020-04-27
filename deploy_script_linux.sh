b1da2b033d2afa4b011bbc03bb7d6c9e8756de18
Fort Zsuzsanna <fortzsu@gmail.com>
2020-04-27 10:42:13 +0000

    Update deploy_script_linux.sh

--- deploy_script_linux.sh
+++ deploy_script_linux.sh
@@ -17,7 +17,8 @@ pem_file_full_path=moovsmart.pem
 
 #BUILD PROJECT
 cd $frontend_folder_name
-ng build --prod
+npm install
+npm run build:prod
 cd ..
 mvn clean package -DskipTests=true
 chmod 400 $pem_file_full_path
