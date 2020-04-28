6039f7c5deba2d490432adae42b1cbe6b322746b
rau <jani.rauschenberger@hotmail.com>
2020-04-28 11:49:16 +0200

    add ws app prefix

--- moovsmart/src/app/services/notification.service.ts
+++ moovsmart/src/app/services/notification.service.ts
@@ -20,14 +20,14 @@ export class NotificationService {
   public onNotification: ReplaySubject<NotificationModel> = new ReplaySubject<NotificationModel>(1);
 
   constructor(private usrService: UserService) {
-    this.socket = new SockJS(environment.BASE_URL + '/secured/notifications');
+    this.socket = new SockJS(environment.BASE_URL + '/notifications');
     this.stompClient = Stomp.over(this.socket);
     this.usrService.loggedIn
       .subscribe(loggedin => loggedin
         ?
         this.usrService.getCurrentUser.subscribe(usr =>
           this.stompClient.connect({}, connection => {
-            this.stompClient.subscribe('/secured/user/queue/notify',
+            this.stompClient.subscribe('/user/queue/notify',
               (msg => this.onNotification.next(JSON.parse(msg.body)))
             );
           }))
