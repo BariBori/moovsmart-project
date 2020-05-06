1af6bd10566f8c0569c5fe73ba4b7e611d706b3f
Bozsó-Fort Zsuzsanna <fortzsu@gmail.com>
2020-05-06 15:43:10 +0200

    Last bid from BE - fixed

--- moovsmart/src/app/components/bid-form/bid-form.component.ts
+++ moovsmart/src/app/components/bid-form/bid-form.component.ts
@@ -15,8 +15,6 @@ export class BidFormComponent implements OnInit {
   advertId: number;
   userId: number;
 
-  // lastBidArray: Array<BidListItemModel>;
-  // lastBid: number;
   minBid: number;
   nextBid: string;
 
@@ -28,38 +26,30 @@ export class BidFormComponent implements OnInit {
     private router: Router,
     private userService: UserService,
     private route: ActivatedRoute
-  ) { }
+  ) {
+  }
 
   bidForm = this.formBuilder.group({
-    amountOfBid : ['']
+    amountOfBid: ['']
   })
 
   ngOnInit(): void {
-      this.advertId = Number(this.route.snapshot.paramMap.get('id'));
-      this.userService.getCurrentUser.subscribe(
-        user => this.userId = user.id
-      );
-      this.getLastBid();
+    this.advertId = Number(this.route.snapshot.paramMap.get('id'));
+    this.userService.getCurrentUser.subscribe(
+      user => this.userId = user.id
+    );
+    this.getLastBid();
   };
 
 
-  onSubmit(){
+  onSubmit() {
     let formData: BidFormDataModel = this.bidForm.value;
     this.bidService.createBid(formData, this.advertId).subscribe(
       () => this.router.navigate(['../property-list']),
     );
   }
 
-  getLastBid(){
-    // this.bidService.getBidList(Number(this.advertId)).subscribe(
-    //   amount => {
-    //     this.lastBidArray = amount.slice(0,1);
-    //     this.lastBid = this.lastBidArray[0].amountOfBid;
-    //     this.minBid = this.lastBid + 0.1;
-    //     this.nextBid = (this.lastBid + 0.1).toFixed(1);
-    //     console.log(this.lastBid);
-    //   },
-    // )
+  getLastBid() {
     this.bidService.getLastBid(this.advertId).subscribe(
       lastAmount => {
         this.lastBidAmount = lastAmount;
