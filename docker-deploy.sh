8d23592c49124378839bd6e3a1e14820ac12c1b8
rau <jani.rauschenberger@hotmail.com>
2020-05-12 17:51:33 +0200

    update deploy script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,6 +1,7 @@
 
 #!/bin/sh
 scp docker-compose.yml $server_user@$remote_address:/home/ec2-user/docker-compose.yml
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker pull $MOOVSMART_REGISTRY/backend-prod:latest  
 docker pull $MOOVSMART_REGISTRY/frontend-prod:latest 
 ssh $server_user@$remote_address 'docker-compose down && docker system prune -f && docker-compose up'
