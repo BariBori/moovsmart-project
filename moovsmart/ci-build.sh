625d091dec1377b9d0cba566d869f6383d10892c
rau <jani.rauschenberger@hotmail.com>
2020-05-12 13:51:45 +0200

    edit nginx conf

--- moovsmart/ci-build.sh
+++ moovsmart/ci-build.sh
@@ -4,7 +4,7 @@ if [ -z ${BACKEND_HOST+x} ] && [ -z ${FRONTEND_HOST+x} ];
     then
         echo "BACKEND_HOST or FRONTEND_HOST is unset, going with default";
     else
-        echo "setting BASE_URL in environment.prod.ts to http://$BACKEND_HOST";
+        echo "setting BASE_URL in environment.prod.ts to http://$FRONTEND_HOST";
         sed  -i "s/BASE_URL: '.*'/BASE_URL: '$BACKEND_HOST'/g" src/environments/environment.prod.ts
         echo "setting upstream backend in nginx.conf to $BACKEND_HOST";
         sed -i  "s/upstream backend {.*}/upstream backend { server $BACKEND_HOST:8080; }/g" nginx.conf
