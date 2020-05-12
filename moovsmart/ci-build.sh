3c90cc0b55b3f1f58f1f59e3aa4254429325f6ff
rau <jani.rauschenberger@hotmail.com>
2020-05-12 18:37:28 +0200

    update ci-build.sh

--- moovsmart/ci-build.sh
+++ moovsmart/ci-build.sh
@@ -8,8 +8,8 @@ if [ -z ${BACKEND_HOST+x} ] && [ -z ${FRONTEND_HOST+x} ];
         sed  -i "s/BASE_URL: '.*'/BASE_URL: '$BACKEND_HOST'/g" src/environments/environment.prod.ts
         echo "setting upstream backend in nginx.conf to $BACKEND_HOST";
         sed -i  "s/upstream backend {.*}/upstream backend { server $BACKEND_HOST:8080; }/g" nginx.conf
-        echo "setting FE server name in nginx.conf to $FRONTEND_HOST";
-        sed -i  "s/server_name .*;/server_name $FRONTEND_HOST;/g" nginx.conf
+        echo "setting FE server name in nginx.conf to http://$FRONTEND_HOST";
+        sed -i  "s/server_name .*;/server_name http://$FRONTEND_HOST;/g" nginx.conf
 fi
 
 ng build --prod
