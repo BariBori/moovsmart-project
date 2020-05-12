7dc2abad604d27e8d11bbb3fcc3ff3440cc671b0
rau <jani.rauschenberger@hotmail.com>
2020-05-12 20:53:00 +0200

    update script

--- moovsmart/ci-build.sh
+++ moovsmart/ci-build.sh
@@ -9,7 +9,7 @@ if [ -z ${BACKEND_HOST+x} ] && [ -z ${FRONTEND_HOST+x} ];
         echo "setting proxy_pass in nginx.conf to $BACKEND_HOST";
         sed -i  "s/backend/$BACKEND_HOST/g" nginx.conf
         echo "setting FE server name in nginx.conf to $FRONTEND_HOST";
-        sed -i  "s/frontend/$FRONTEND_HOST;/g" nginx.conf
+        sed -i  "s/frontend/$FRONTEND_HOST/g" nginx.conf
 fi
 
 ng build --prod
