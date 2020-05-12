f18d719b2491b2afaab92e50bec90d941708f17b
rau <jani.rauschenberger@hotmail.com>
2020-05-12 20:42:23 +0200

    update script

--- moovsmart/ci-build.sh
+++ moovsmart/ci-build.sh
@@ -4,12 +4,12 @@ if [ -z ${BACKEND_HOST+x} ] && [ -z ${FRONTEND_HOST+x} ];
     then
         echo "BACKEND_HOST or FRONTEND_HOST is unset, going with default";
     else
-        echo "setting BASE_URL in environment.prod.ts to http://$FRONTEND_HOST";
-        sed  -i "s/BASE_URL: '.*'/BASE_URL: '$BACKEND_HOST'/g" src/environments/environment.prod.ts
+        echo "setting BASE_URL in environment.prod.ts to $FRONTEND_HOST";
+        sed  -i "s/BASE_URL: '.*'/BASE_URL: '$FRONTEND_HOST'/g" src/environments/environment.prod.ts
         echo "setting proxy_pass in nginx.conf to $BACKEND_HOST";
-        sed -i  "s/proxy_pass.*/proxy_pass http://$BACKEND_HOST:8080; }/g" nginx.conf
+        sed -i  "s/backend/$BACKEND_HOST/g" nginx.conf
         echo "setting FE server name in nginx.conf to $FRONTEND_HOST";
-        sed -i  "s/server_name.*/server_name $FRONTEND_HOST;/g" nginx.conf
+        sed -i  "s/frontend/$FRONTEND_HOST;/g" nginx.conf
 fi
 
 ng build --prod
